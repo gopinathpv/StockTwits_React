677615fcb87c34c93dfe104be86ac8ff4722eb4b
gopinathpv <gopinpv3@gmail.com>
2020-01-06 14:56:44 -0600

    Package_Modify

--- server.js
+++ server.js
@@ -44,3 +44,12 @@ app.use((err,req,res,next)=>{
       }
   })
 })
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+}
+
+app.get("*",(req,res) => {
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+})
