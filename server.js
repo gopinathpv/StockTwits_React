219ab8c62722a41e4c34a57199c0f38029efe2d4
gopinathpv <gopinpv3@gmail.com>
2020-01-06 16:06:02 -0600

    Package_Modi2

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const fetch = require("node-fetch");
-const bodyParser = require("body-parser");
 const cors = require('cors')
 const path  = require('path')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 
-app.post("/data", function(req, res) {
+app.post("/data", cors(),function(req, res) {
   var inputdata = req.body.value;
   var jsondata;
   console.log("data",inputdata)
@@ -24,30 +21,15 @@ app.post("/data", function(req, res) {
     });
 });
 
-app.use((req,res,next)=>{
-  const error = new Error('Not Found') 
-  error.status(404) 
-  next(error)
-}) 
-
-app.use((err,req,res,next)=>{
-  res.status(err.status || 500) 
-  res.json({
-      err: {
-          message :err.message
-      }
-  })
-})
-
 app.use(cors());
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static('client/build'));
-  
-  app.get("*",(req,res) => {
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-  })
-}
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
