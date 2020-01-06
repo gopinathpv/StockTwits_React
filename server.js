64065ca2fec01abcd73c05793a761f703d6db23e
gopinathpv <gopinpv3@gmail.com>
2020-01-06 17:45:55 -0600

    lst

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (req, res) => {
   res.send('YOUR EXPRESS BACKEND IS CONNECTED TO REACT' );
@@ -46,7 +44,10 @@ app.use((err,req,res,next)=>{
 })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
