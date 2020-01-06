9de2c3652a7464d813af0c1479d6639ee04f6053
gopinathpv <gopinpv3@gmail.com>
2020-01-06 17:30:55 -0600

    lst

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((err,req,res,next)=>{
 })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'client\build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
 });
