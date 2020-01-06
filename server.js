c0203d6d5aa57d82318d074f1027b0a48c76c620
gopinathpv <gopinpv3@gmail.com>
2020-01-06 15:48:10 -0600

    Package_Modify2

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
 const cors = require('cors')
+const path  = require('path')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 5000;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.post("/data", function(req, res) {
   var inputdata = req.body.value;
@@ -45,8 +43,11 @@ app.use(cors());
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static('client/build'));
-
-app.get("*",(req,res) => {
-  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-})
+  
+  app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
 }
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
